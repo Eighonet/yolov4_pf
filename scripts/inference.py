ef91f68009588260c9450e664248368cb702d57c
Porvatov Vadim <eighonet@gmail.com>
2021-12-20 22:44:52 +0300

    fix

--- scripts/inference.py
+++ scripts/inference.py
@@ -3,6 +3,6 @@ import cv2, os
 def simple_inference(threshold = 0.05, path_to_image = "yolov4/examples/", image = "parking_test.jpg") -> None:
     inference = "./darknet detector test\
      ../yolov4/data/obj.data ../yolov4/cfg/yolov4-obj.cfg ../yolov4/weights/yolov4-obj_best.weights ../"\
-    + path_to_image + image + " -thresh" + str(threshold)
+    + path_to_image + image + " -thresh " + str(threshold)
     
     os.system("cd darknet && " + inference)
