5fab7791f3ad61c009157e5b6f10a0e463b8aba2
Porvatov Vadim <eighonet@gmail.com>
2022-03-21 22:27:01 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
-#!/bin/sh
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1YocbelRjhwUGYcIXxQsWzwVK4UbKCI01' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1YocbelRjhwUGYcIXxQsWzwVK4UbKCI01" -O yolov4-obj_best.weights && rm -rf /tmp/cookies.txt
 mkdir yolov4/weights
 mv yolov4-obj_best.weights yolov4/weights/yolov4-obj_best.weights
